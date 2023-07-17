e9a662b65974c2b621545b6f8c53c066bcb1a0b8
space8033 <space8237@naver.com>
2023-07-17 18:44:09 +0900

    Initialize

---
+++ src/main/java/com/mycompany/springhomework/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.mycompany.springhomework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.springhomework.controller.HomeController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		log.info("실행1");
+		log.info("실행2");
+		log.info("실행3");
+		
+		return "index";
+	}
+}
