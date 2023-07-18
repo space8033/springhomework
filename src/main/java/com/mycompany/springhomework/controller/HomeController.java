06afb8885fd2bde7920b13e9503300b7fdf2bf1e
space8033 <space8237@naver.com>
2023-07-18 10:17:55 +0900

    log 추가

--- src/main/java/com/mycompany/springhomework/controller/HomeController.java
+++ src/main/java/com/mycompany/springhomework/controller/HomeController.java
@@ -13,6 +13,9 @@ public class HomeController {
 	@RequestMapping("/")
 	public String index() {
 		log.info("실행1");
+		log.info("실행2");
+		log.info("실행3");
+		log.info("실행5");
 		
 		return "index";
 	}
